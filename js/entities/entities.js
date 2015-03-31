c010e0601cbb3e597c5710f6455592535640749b
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-03-30 23:18:09 -0700

    video 31

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -54,32 +54,12 @@ game.PlayerEntity = me.Entity.extend({
     update: function(delta) {
         this.now = new Date().getTime();
           
-        if (this.health <= 0) {
-            this.dead = true;    
-        }    
+       this.dead = checkIfDead();  
             
-         if (me.input.isKeyPressed("right")) {
-            //adds to the position of my x by  the velocity defined above in
-            //setVelocity() and multiplying it by me.timer.tick.
-            //me.timer.tick makes the movement look smooth
-            this.body.vel.x += this.body.accel.x * me.timer.tick;
-            this.facing = "right";
-            this.flipX(true);
-        } else if (me.input.isKeyPressed("left")) {
-            this.facing = "left";
-            this.body.vel.x -= this.body.accel.x * me.timer.tick;
-            this.flipX(false);
-        } else {
-            this.body.vel.x = 0;
-        }
-
-        if (me.input.isKeyPressed("jump") && !this.jumping && !this.falling) {
-            this.jumping = true;
-            this.body.vel.y -= this.body.accel.y * me.timer.tick;
-        }
-
-
-        if (me.input.isKeyPressed("attack")) {
+        this.checkKeyPressesAndMove(); 
+        
+      
+            if (me.input.isKeyPressed("attack")) {
             if (!this.renderable.isCurrentAnimation("attack")) {
                 //Sets the current animation to attack and once that  is over
                 //goes back to  the idle animation
@@ -101,8 +81,46 @@ game.PlayerEntity = me.Entity.extend({
         me.collision.check(this, true, this.collideHandler.bind(this), true);
         this.body.update(delta);
 
+        //console.log (" Dead x " + this.body.pos.x " y " + this.body.pos.y);
+        
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    checkIfDead: function() {
+            if (this.health <= 0) {
+            return true;    
+        }  
+        return false;
+    },
+    
+    checkKeyPressesAndMove: function(){
+           if (me.input.isKeyPressed("right")) {
+               this.moveRight();
+        } else if (me.input.isKeyPressed("left")) {
+            this.moveLeft();
+        } else {
+            this.body.vel.x = 0;
+        }
+
+       if (me.input.isKeyPressed("jump") && !this.jumping && !this.falling) {
+            this.jump();
+        }
+    },
+    
+    moveRight: function() {
+        
+    },
+    
+    moveLeft: function(){
+         this.facing = "left";
+         this.body.vel.x -=this.body.accel.x * me.timer.tick;
+         this.flipX(false);
+    },
+    
+    jump: function(){
+        this.jumping = true;
+        this.body.vel.y -= this.body.accel.y * me.timer.tick;
     },
     
     loseHealth: function(damage){
