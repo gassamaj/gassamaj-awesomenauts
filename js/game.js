2a90ff3ed17378f76b7c5715b4f3d9ad441e9c68
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-04-03 13:58:55 -0700

    video 42

--- js/game.js
+++ js/game.js
@@ -6,8 +6,8 @@ var game = {
 	data : {
 		// score
 		score : 0,
-                enemyBaseHealth: 10,
-                playerBaseHealth: 10,
+                enemyBaseHealth: 1,
+                playerBaseHealth: 1,
                 enemyCreepHealth: 10,
                 playerHealth: 10,
                 enemyCreepAttack: 1,
@@ -49,7 +49,10 @@ var game = {
 	}
         
         me.save.add({exp: 0, exp1: 0, exp2: 0,  exp3:  0, exp4: 0});
-
+         
+         console.log(game.data.exp);
+         console.log(game.data.exp2);
+         
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
 
