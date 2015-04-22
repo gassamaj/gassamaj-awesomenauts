2ed366a1bc3a803f66b5dbaf85c2ba8fec9a70a0
Joshua Gassama <JoshuaGassama@yahoo.com>
2015-04-22 13:59:21 -0700

    video 51

--- js/entities/gamemanagers/GameManager.js
+++ js/entities/gamemanagers/GameManager.js
@@ -1,50 +1,3 @@
-game.GameTimerManager = Object.extend({
-    init: function(x, y, settings){
-        this.now = new Date().getTime();
-        this.lastCreep = new Date().getTime();
-        this.paused = false;
-        this.alwaysUpdate = true;
-    },
-    
-    update: function() {
-        this.now = new Date().getTime();
-        this.goldTimerCheck();
-        this.creepTimerCheck();
-        
-        return true;
-    },
-    
-    goldTimerCheck: function(){
-        if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)){
-           game.data.gold += (game.data.exp1+1);
-           console.log("Current gold: " + game.data.gold);
-        }
-    },
-    
-    creepTimerCheck: function(){
-           if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
-            this.lastCreep = this.now;
-            var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
-            me.game.world.addChild(creepe, 5);
-        }
-    }
-    });
-
-game.HeroDeathManager = Object.extend({
-    init: function (x, y, settings) {
-        this.alwaysUpdate = true;
-    },
-    
-    update: function(){
-        if(game.data.player.dead){
-            me.game.world.removeChild(game.data.player);
-            me.state.current().resetPlayer(10, 0);
-        }
-        
-        return true;
-    }
-});
-
 game.ExperienceManager = Object.extend({
     init: function (x, y, settings){
         this.alwaysUpdate = true;
@@ -208,7 +161,7 @@ game.SpendGold = Object.extend({
               if(skill === 1){
               game.data.gold -= ((game.data.skill1 +1) * 10);
               game.data.skill1 += 1;
-              game.data.player.attack += 1;
+              game.data.player.Attack += 1;
           }else if(skill ===2){
               game.data.gold -= ((game.data.skill2 +1) * 10);
               game.data.skill2 += 1;
@@ -222,10 +175,9 @@ game.SpendGold = Object.extend({
               game.data.gold -= ((game.data.ability2 +1) * 10);
               game.data.ability3 += 1;
           }else if (skill ===6){
-              
-          }else if (skill ===3){
-              
-          }
+             game.data.gold -= ((game.data.ability3 +1)* 10);    
+             game.data.ability3 += 1;
+              }
           }
           
       
